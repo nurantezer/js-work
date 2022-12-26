4b48b42fb76bd9908d3638d471aa4c32f011a519
nurantezer <nurantezer68@gmail.com>
2022-12-26 08:50:41 +0300

    while

--- conditions/index.js
+++ conditions/index.js
@@ -325,3 +325,29 @@ okul puanı nax 60 veriyor
 //     console.log(i);
 // }
 // console.log(`Toplam: ${toplam}`);
+                 
+                  //while
+// let sayac = 1;
+// while(sayac <=10){
+//     console.log(sayac);
+//     sayac++;
+// }
+
+// let sayac = 0;
+// while(sayac <= 10){
+//     if(sayac%2 == 0){
+//         console.log(sayac);
+//     }
+//     sayac++;
+// }
+
+let sayac = 1;
+while (true) {
+    console.log(sayac);
+    if(sayac == 7){
+        break;
+    }
+    
+    sayac++;
+    
+}
