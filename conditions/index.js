12edfa3992f00e0745955810db0a25ae2673d586
nurantezer <nurantezer68@gmail.com>
2022-12-26 08:06:46 +0300

    update

--- conditions/index.js
+++ conditions/index.js
@@ -299,7 +299,29 @@ okul puanı nax 60 veriyor
 // for(let i=1 ; i<=10 ; i++){
 //     console.log(i);
 // }
+
 //1 den 10 a kadar ciftleri yazdıralım
 // for(let i=0 ; i<=10 ; i=i+2){
 //     console.log(i);
 // }
+
+//1 den 10 a kadar tekleri yazdıralım
+// for(let i=1 ; i<=10 ; i+=2){
+//     console.log(i);
+// }
+
+// for(let i=1; i<=10 ; i++){
+//     if(i%2==1){
+//         console.log("Nuran");
+//     }else{
+//         console.log("Naber");
+//     }
+// }
+
+//50den 1e kadar gidelim ve toplamlarını bulalım
+// let toplam = 0;
+// for(let i=50 ; i>=1 ; i--){
+//     toplam+=i;
+//     console.log(i);
+// }
+// console.log(`Toplam: ${toplam}`);
