09af709bf9436011fd26d6f4dfe63d1c1cf35c63
nurantezer <nurantezer68@gmail.com>
2022-12-26 17:32:59 +0300

    update

--- fonction/index.js
+++ fonction/index.js
@@ -30,9 +30,73 @@
 // console.log(elif);
 // console.log(calculate("Rose",1990));
 
-function oddOrEven(num) {
-    return num % 2 ? "ODD" : "EVEN"
+// function oddOrEven(num) {
+//     return num % 2 ? "ODD" : "EVEN"
+// }
+// const number = +prompt("please enter a number to check odd or even:");
+// const oddEven = oddOrEven(number)
+// console.log(`${number} is ${oddEven}`);
+
+// const printHello = function (){
+//     console.log("Hello");
+// }
+// printHello() //!invoke, call
+
+// let oddOrEven = function(num){
+//     return num % 2 === 0 ? "EVEN" : "ODD"
+// }
+// console.log(oddOrEven(5));
+
+// const findBiggest = function (n1, n2, n3){
+//     let biggest =n1
+//     if(n2 >= biggest){
+//         biggest = n2
+//     }
+    
+//     if(n3 >= biggest){
+//         biggest = n3
+//     }
+//     return biggest
+// }
+// console.log(`The Biggest:${findBiggest(2,1,4)}`);
+
+//!iç içe (nested) fonksiyon örnek olarak yaptık
+//!Örnek3: Bir fonksiyon içerisinde baska fonksiyonlar cagrılabilir
+const add =function (n1,n2){
+    return n1 + n2
 }
-const number = +prompt("please enter a number to check odd or even:");
-const oddEven = oddOrEven(number)
-console.log(`${number} is ${oddEven}`);
+const sub = function(n1,n2){
+    return n1 - n2
+}
+const div = function (n1,n2){
+    return n1 / n2
+}
+const mul = function (n1,n2){
+    return n1 * n2
+}
+
+//?bunu başkabir fonksiyonda çağırıyorum
+ const compute = function (n1, n2, op) {
+   let result = 0;
+   switch (op) {
+     case "+":
+       result = add(n1, n2);
+       break;
+     case "-":
+       result = sub(n1, n2);
+       break;
+     case "/":
+       result = div(n1, n2);
+       break;
+     case "*":
+       result = mul(n1, n2);
+       break;
+     default:
+       break;
+   }
+   return result;
+ };
+
+ console.log(compute(4, 3, "/"));
+ console.log(compute(2, 4, "*"));
+ console.log(compute(3, 1, "+"));
