ed05f091099f6d6fc3c0e85ce63ba4c9ce4cf79b
nurantezer <nurantezer68@gmail.com>
2022-12-28 20:36:46 +0300

    update

--- string/index.js
+++ string/index.js
@@ -1,5 +1,14 @@
-let str4 = new String("This is a string")
-console.log(str4);
-console.log(str4.length);
-let str3 = `This is a string`
-console.log(str3);
+// let str4 = new String("This is a string")
+// console.log(str4);
+// console.log(str4.length);
+// let str3 = `This is a string`
+// console.log(str3);
+
+// let str1 = "Clarusway"
+// let str2 = `Full Stack`
+// console.log(str1.concat(" ", str2));
+// let str5 = str1 +" "+ str2
+// console.log(str5);
+
+let str = 'Hello world'
+console.log(str[str.length-1]);
