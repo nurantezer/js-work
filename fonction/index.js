3b45b03eb8e38f8f40e8ffc1007a3fa244358716
nurantezer <nurantezer68@gmail.com>
2022-12-29 12:54:28 +0300

    break

--- fonction/index.js
+++ fonction/index.js
@@ -60,43 +60,52 @@
 // }
 // console.log(`The Biggest:${findBiggest(2,1,4)}`);
 
-//!iç içe (nested) fonksiyon örnek olarak yaptık
-//!Örnek3: Bir fonksiyon içerisinde baska fonksiyonlar cagrılabilir
-const add =function (n1,n2){
-    return n1 + n2
-}
-const sub = function(n1,n2){
-    return n1 - n2
-}
-const div = function (n1,n2){
-    return n1 / n2
-}
-const mul = function (n1,n2){
-    return n1 * n2
-}
+// //!iç içe (nested) fonksiyon örnek olarak yaptık
+// //!Örnek3: Bir fonksiyon içerisinde baska fonksiyonlar cagrılabilir
+// const add =function (n1,n2){
+//     return n1 + n2
+// }
+// const sub = function(n1,n2){
+//     return n1 - n2
+// }
+// const div = function (n1,n2){
+//     return n1 / n2
+// }
+// const mul = function (n1,n2){
+//     return n1 * n2
+// }
 
-//?bunu başkabir fonksiyonda çağırıyorum
- const compute = function (n1, n2, op) {
-   let result = 0;
-   switch (op) {
-     case "+":
-       result = add(n1, n2);
-       break;
-     case "-":
-       result = sub(n1, n2);
-       break;
-     case "/":
-       result = div(n1, n2);
-       break;
-     case "*":
-       result = mul(n1, n2);
-       break;
-     default:
-       break;
-   }
-   return result;
- };
+// //?bunu başkabir fonksiyonda çağırıyorum
+//  const compute = function (n1, n2, op) {
+//    let result = 0;
+//    switch (op) {
+//      case "+":
+//        result = add(n1, n2);
+//        break;
+//      case "-":
+//        result = sub(n1, n2);
+//        break;
+//      case "/":
+//        result = div(n1, n2);
+//        break;
+//      case "*":
+//        result = mul(n1, n2);
+//        break;
+//      default:
+//        break;
+//    }
+//    return result;
+//  };
 
- console.log(compute(4, 3, "/"));
- console.log(compute(2, 4, "*"));
- console.log(compute(3, 1, "+"));
+//  console.log(compute(4, 3, "/"));
+//  console.log(compute(2, 4, "*"));
+//  console.log(compute(3, 1, "+"));
+
+let sayac = 1
+while (sayac<=10) {
+  console.log(sayac);
+  if(sayac==8){
+    break;
+  }
+  sayac++
+}
