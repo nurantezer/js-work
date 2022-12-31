dcae13ff49c52e8235e7951933a1f1bc4965b35f
nurantezer <nurantezer68@gmail.com>
2022-12-31 14:47:52 +0300

    declaration

---
+++ fonction/function.js
@@ -0,0 +1,26 @@
+// function printHello() {
+//     console.log("hello");
+// }
+// printHello()
+
+// function greet(firstName, lastName) {
+//     console.log(`hi, ${firstName} ${lastName}`);
+// }
+// greet("nuran","tezer")
+
+// function calculate(name,yob) {
+//     const age =new Date().getFullYear() - yob
+//     return `${name}'s age is ${age}`
+// }
+// const elif = calculate("elif",2000)
+// console.log(elif);
+// console.log(calculate("rose",1990));
+
+// console.log(oddOrEven(76));
+// function oddOrEven(num) {
+//     return num % 2 ? "odd" : "even"
+// }
+
+ 
+
+
