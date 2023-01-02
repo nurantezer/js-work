2bb9fb6e4a50e510019858b90d9bc02c0b87e7d7
nurantezer <nurantezer68@gmail.com>
2023-01-02 16:55:51 +0300

    string end

--- string/index.js
+++ string/index.js
@@ -39,3 +39,53 @@ console.log(word.indexOf(`be`));//baş
 console.log(word.lastIndexOf(`be`));//en son
 console.log(word.lastIndexOf(`BE`));//-1 bulamadığı zaman döndürüyor.
 
+//startWidth(), endWith()-case sensetive
+const word2 = "Salına salına sinsice :!"
+console.log(word2.startsWith("sa"));
+console.log(word2.startsWith("sa",7));
+console.log(word2.endsWith(`!`));
+
+//replace(searchFor, replaceWith)--immutabele
+//let oku = `Oku bBaban gibi ,saf olma`;
+// oku = oku.replace(`saf olma`,`akıllı ol`) let ile yeniden atama yapıp değiştirebilirim.
+let oku = "Oku Baban gibi, saf olma";
+console.log(oku.replace("saf olma", `akıllı ol`));
+oku = oku.replace(`saf olma`, `akilli ol`)
+console.log(oku);
+
+//detaylı değiştirme alternatifleri için regex kullanılabilir
+console.log(oku.replace(/AKILLI/i, `Zeki`));
+
+
+//slice(beginIndex[, endIndex])
+//substring(beginIndex,[, endIndex])
+//substr(depreaced)
+//bunlarda immutable
+
+const veysel = "Uzun ince bir yoldayım yürüyorum gündez gece..";
+const sliced = veysel.slice(33)
+console.log(sliced,typeof sliced);
+
+console.log(veysel.slice(17,30));
+console.log(veysel.slice(-10));// -1 den başlıyor
+console.log(veysel.slice(-23, -19));
+//substring negatif index kullanmaz.
+console.log(veysel.substring(17,30));
+
+//split([sep [,limit]])
+const tarkan = "gel gündüzle gece olalım";
+const splited = tarkan.split(` `)//boşluklara göre ayırdı ve Arraya çevirdi.
+console.log(splited, typeof splited);
+const gece = splited[2]//indisleme ile değer okunabilir.
+console.log(gece);
+
+const chars =tarkan.split('')
+console.log(chars);// null karakterine göre harfleri ayırarak bir char dizisi oluşturdu.
+
+const copyTarkan = tarkan.split()
+console.log(copyTarkan);//string'i array haline getirdi.tek elemanlı.
+
+//trim()
+const ramazan = "       Hoş geldin ya şehri Ramazan"
+console.log(ramazan.length);
+console.log(ramazan.trim().length);//boşlukları kaldırdı.
