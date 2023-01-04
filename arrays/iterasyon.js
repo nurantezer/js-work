a647f46bde497c309ddb8e11e7c26990d7cc3b7c
nurantezer <nurantezer68@gmail.com>
2023-01-04 08:26:17 +0300

    for in

---
+++ arrays/iterasyon.js
@@ -0,0 +1,22 @@
+const islemler = [-3000, 4500, -2200, -6000, 9000];
+
+const islemlerToplami = () => {
+    let yatirma = 0
+    cekme = 0;
+    for ( let i =0; i < islemler.length; i++){
+        if (islemler[i] >= 0){
+            yatirma += islemler[i];
+        }else{
+            cekme += islemler[i];
+        }
+    }
+    return `Toplam yatirma: ${yatirma}, Toplam cekme: ${-cekme}`
+}
+console.log(islemlerToplami());
+
+//* FOR-IN LOOP
+//* sORU: ögrenciler dizisinde ögrenci isimleri saklanmaktadir.
+//*ögrencileri aramamamizi saglayacak ve aranılan ögrenciden 
+//*ne kadar sayida bulundugunu ana programa döndürecek bir
+//*fonksiyonu yazınız. Eger aranılan isimde bir ögrenci yok ise 
+//*fonksiyon aranılan ögrenci bulunamadı döndürmelidir.
