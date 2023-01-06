45003bea966727cd0ad62a5b122fcfb36ead8ca2
nurantezer <nurantezer68@gmail.com>
2023-01-06 13:07:59 +0300

    son

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -99,17 +99,32 @@
 
 const numbers = [3, 5, 2, "4", "four", 3, "4", "bes", "1", 3];
 
-console.log(numbers.includes(4));
-console.log(numbers.includes("4"));
+// console.log(numbers.includes(4));
+// console.log(numbers.includes("4"));
 
-console.log(numbers.indexOf(3));
-console.log(numbers.lastIndexOf(3));
-console.log(numbers.lastIndexOf(7));
+// console.log(numbers.indexOf(3));
+// console.log(numbers.lastIndexOf(3));
+// console.log(numbers.lastIndexOf(7));
 
-const strNumbers = numbers.join("-");
-console.log(strNumbers);
+// const strNumbers = numbers.join("-");
+// console.log(strNumbers);
 
 let data = [3, 1, 5, 7, 8, 910, 22, 3];
 
-data = data.sort((a, b) => b - a).join("~~ ");
-console.log(data);
+// data = data.sort((a, b) => b - a).join("~~ ");
+// console.log(data);
+
+console.log(numbers.toString());
+
+const cars = ["BMW", "mercedes", "fiat", "anadol"];
+console.log(cars.slice(2, 3));
+console.log(cars.slice(2));
+
+const newCars = cars.slice(3, 4);
+console.log(newCars, cars);
+
+const message = ["The weather", "is", "very", "nice"];
+const now = [new Date().toLocaleString()];
+
+const concattedMsg = message.concat(now);
+console.log(concattedMsg);
