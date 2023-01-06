b714f79f57332e4fa9f683bae0fd03d5d8472ed6
nurantezer <nurantezer68@gmail.com>
2023-01-06 14:42:01 +0300

    map end

--- arrays/iterasyon.js
+++ arrays/iterasyon.js
@@ -160,3 +160,14 @@ console.log(upperNames);
 console.log(names);
 
 console.log(names.map((name) => name.toLocaleUpperCase()));
+
+//*tlPrices dizisindeki rakamların Euro ve dolar karşılıklarını hesaplatarak
+//*yeni dizilere kaydediniz
+
+const euro = 18.23;
+const dolar = 18.19;
+const tlPrices = [100, 150, 100, 50, 80];
+
+const euroPrices = tlPrices.map((tl) => Number((tl / euro).toFixed(2)));
+const dolarPrices = tlPrices.map((tl) => Number((tl / dolar).toFixed(2)));
+console.log(euroPrices, dolarPrices);
