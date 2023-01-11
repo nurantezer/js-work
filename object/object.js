156da4ac0c3dbbba42d796b97c6b73142233ebdf
nurantezer <nurantezer68@gmail.com>
2023-01-11 14:12:48 +0300

    json

--- object/object.js
+++ object/object.js
@@ -57,20 +57,90 @@
 
 //*Object Metotları
 
-const personel = {
-  name: "can",
-  surname: "canan",
-  dob: "1990",
-  salary: 10000,
-  job: "developer",
-  drivingLicence: false,
-  calculateAge: function () {
-    return new Date().getFullYear() - this.dob;
+// const personel = {
+//   name: "can",
+//   surname: "canan",
+//   dob: "1990",
+//   salary: 10000,
+//   job: "developer",
+//   drivingLicence: false,
+//   calculateAge: function () {
+//     return new Date().getFullYear() - this.dob;
+//   },
+//   summary: function () {
+//     return `${this.name} is ${this.calculateAge()} years old.`;
+//   },
+// };
+// console.log(personel.dob);
+// console.log("age:", personel.calculateAge());
+// console.log(personel.summary());
+
+const people = {
+  person1: {
+    name: "Can",
+    surname: "Canan",
+    dob: "1990",
+    job: "developer",
+    salary: "140000",
+    drivingLicense: true,
   },
-  summary: function () {
-    return `${this.name} is ${this.calculateAge()} years old.`;
+  person2: {
+    name: "John",
+    surname: "Sweet",
+    dob: "1990",
+    job: "tester",
+    salary: "110000",
+    drivingLicense: false,
   },
+  person3: {
+    name: "Steve",
+    surname: "Job",
+    dob: "2000",
+    job: "developer",
+    salary: "90000",
+    drivingLicense: true,
+  },
+  person4: "JS",
 };
-console.log(personel.dob);
-console.log("age:", personel.calculateAge());
-console.log(personel.summary());
+
+//*person2'nin adını ve maaşını yazdırınız
+
+// console.log(people);
+// console.log(people.person2.name);
+// console.log(people.person2.salary);
+
+//*people objesindeki tüm salaryleri yazdırın
+
+// for (let person in people) {
+//   console.log(person);
+//   console.log(people[person].salary);
+//   console.log(people[person].name);
+// }
+
+// console.log(Object.keys(people));
+// console.log(Object.values(people));
+// console.log(Object.entries(people));
+
+// for (let key of Object.keys(people)) {
+//   console.log(key);
+// }
+
+// for (let v of Object.values(people)) {
+//   console.log(v);
+// }
+
+// for (let [k, v] of Object.entries(people)) {
+//   console.log("key:", k, "value:", v.job);
+// }
+
+// Object.keys(people).forEach((p) => console.log(p));
+// Object.values(people).forEach((p) => console.log(p.name));
+
+Object.values(people)
+  .filter((p) => p.job === "developer")
+  .forEach((p) => console.log(p.dob));
+
+const dobs = Object.values(people)
+  .filter((p) => p.job === "developer")
+  .map((p) => p.dob);
+console.log(dobs);
