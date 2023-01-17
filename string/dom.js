b3382a601dd79a04f12b2929b49afb6f09a922fd
nurantezer <nurantezer68@gmail.com>
2023-01-17 14:01:11 +0300

    add

---
+++ string/dom.js
@@ -0,0 +1,61 @@
+// console.log("******SELECTORS********");
+// document.title = "nuran"
+// const myHeader = document.getElementById("header")
+// myHeader.style.background = "red"
+// myHeader.style.color = "yellow"
+
+// const myButon = document.getElementById("btn")
+// myButon.style.padding = "1rem"
+// myButon.style.color = "blue"
+
+// const list = document.getElementsByTagName("li")
+// list[0].style.color = "red"
+// list[list.length-1].style.color = "purple"
+// list[3].style.color = "magenta"
+// list[2].style.color = "pink"
+// console.log(list.item(3));
+// console.log(list[2].textContent);
+// console.log(list[2].innerText);
+// console.log(list[2].innerHTML);
+
+// list[4].innerHTML += `<li><a class="link" href="https://www.clarusway.com">CW</a></li>`;
+
+// list[3].innerHTML += `<p>Nuran</p>`
+// list[2].innerHTML += `<h1>DOM</h1>`
+
+// const itemList = document.getElementsByClassName("list")
+// console.log(itemList);
+
+// const itemListArr = Array.from(itemList)
+// console.log(...itemList);
+// itemListArr.forEach((x) => console.log(x))
+
+// const itemListSpread = [...itemList]
+// console.log(itemListSpread);
+// itemListSpread.map((x) => console.log((x.style.color = "red")))
+
+// const addBtnType = document.querySelector("input[type='button']");
+// console.log(addBtnType.value);
+
+const lists = document.querySelectorAll("ul li"); //? tagname
+console.log(lists); //? Nodelist
+
+lists.forEach((li) => console.log(li.innerText));
+
+console.log(
+  document.querySelectorAll("section.item-list ul li:nth-child(4)")[0].innerText
+);
+
+console.log(
+  document.querySelector("section.item-list ul li:nth-child(4)").innerText
+);
+
+document.querySelector("section.item-list ul li:nth-child(4)").innerHTML =
+  "<p>Paragraf</p>";
+
+
+
+
+
+
+
