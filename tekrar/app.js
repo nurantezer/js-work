6f2f31901d7e6357074c1df3ead3a81604d6ef59
nurantezer <nurantezer68@gmail.com>
2023-01-31 08:45:24 +0300

    oop

---
+++ tekrar/app.js
@@ -0,0 +1,22 @@
+// let sayi = 123
+// let $sayi = 123
+// let _sayi = 123
+
+console.error("aaaaaaa")
+console.warn("aaaaaaa")
+console.clear();
+
+console.time("id")
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.log("nuran");
+console.timeEnd("id")
+
